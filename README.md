5934ef7bd3161231438e6ab7967b4210fdde5688
Benedikt Schatz <schlchtwtrfrnt@gmail.com>
2017-07-25 12:06:12 +0200

    Update readme and docs

--- README.md
+++ README.md
@@ -1,34 +1,51 @@
 # sublime_fuzzy
 
-Fuzzy matching algorithm based on Sublime Text's string search.
+Fuzzy matching algorithm based on Sublime Text's string search. Iterates through characters of a search string and calculates a score based on matching consecutive/close groups of characters. Tries to find the best match by scoring multiple match paths.
 
-## Usage
+### Documentation
+
+
+
+### Usage
 
 Basic usage:
 
-    let s = "some search thing";
-    let search = "something";
-    let result = best_match(search, s).unwrap();
+```rust
+use sublime_fuzzy::best_match;
+
+let s = "some search thing";
+let search = "something";
+let result = best_match(search, s).unwrap();
+
+// Output: score: 368
+println!("score: {:?}", result.score());
+```
 
 Simple formatting:
 
-    let s = "some search thing";
-    let search = "something";
-    let result = best_match(search, s).unwrap();
-    
-    // Will output '<span>some</span> search <span>thing</span>'.
-    println!("formatted: {:?}", format_simple(&result, s, "<span>", "</span>"));
+```rust
+use sublime_fuzzy::{best_match, format_simple};
+
+let s = "some search thing";
+let search = "something";
+let result = best_match(search, s).unwrap();
+
+// Output: <span>some</span> search <span>thing</span>
+println!("formatted: {:?}", format_simple(&result, s, "<span>", "</span>"));
+```
 
 Adjust scoring:
 
-    use sublime_fuzzy::FuzzySearcher;
-    
-    let mut search = FuzzySearcher::new();
-    
-    search.set_search("something");
-    search.set_target("some search thing");
-    
-    // Weight consecutive matching chars less.
-    search.set_score_consecutive(4);
-    
-    println!("result: {:?}", search.best_match());
+```rust
+use sublime_fuzzy::FuzzySearcher;
+
+let mut search = FuzzySearcher::new();
+
+search.set_search("something");
+search.set_target("some search thing");
+
+// Weight consecutive matching chars less.
+search.set_score_consecutive(4);
+
+println!("result: {:?}", search.best_match());
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,55 @@
-#![allow(dead_code)]
+/*!
+
+Fuzzy matching algorithm based on Sublime Text's string search. Iterates through
+characters of a search string and calculates a score based on matching
+consecutive/close groups of characters. Tries to find the best match by scoring
+multiple match paths.
+
+# Usage
+
+Basic usage:
+
+```rust
+use sublime_fuzzy::best_match;
+
+let s = "some search thing";
+let search = "something";
+let result = best_match(search, s).unwrap();
+
+// Output: score: 368
+println!("score: {:?}", result.score());
+```
+
+Simple formatting:
+
+```rust
+use sublime_fuzzy::{best_match, format_simple};
+
+let s = "some search thing";
+let search = "something";
+let result = best_match(search, s).unwrap();
+
+// Output: <span>some</span> search <span>thing</span>
+println!("formatted: {:?}", format_simple(&result, s, "<span>", "</span>"));
+```
+
+Adjust scoring:
+
+```rust
+use sublime_fuzzy::FuzzySearcher;
+
+let mut search = FuzzySearcher::new();
+
+search.set_search("something");
+search.set_target("some search thing");
+
+// Weight consecutive matching chars less.
+search.set_score_consecutive(4);
+
+println!("result: {:?}", search.best_match());
+```
+
+*/
 
 use std::cmp::{max, Ordering};
 
@@ -275,7 +326,8 @@ fn match_chains(search: &str, target: &str, search_offset: usize, mut index: usi
 ///     let search = "something";
 ///     let result = best_match(search, s).unwrap();
 ///
-///     println!("score: {:?} matches: {:?}", result.score(), result.matches());
+///     // Output: score: 368
+///     println!("score: {:?}", result.score());
 ///
 pub fn best_match(search: &str, target: &str) -> Option<SearchResult> {
     let mut searcher = FuzzySearcher::new();
@@ -298,6 +350,7 @@ pub fn best_match(search: &str, target: &str) -> Option<SearchResult> {
 ///     let search = "something";
 ///     let result = best_match(search, s).unwrap();
 ///     
+///     // Output: <span>some</span> search <span>thing</span>
 ///     println!("formatted: {:?}", format_simple(&result, s, "<span>", "</span>"));
 ///
 pub fn format_simple(result: &SearchResult, string: &str, before: &str, after: &str) -> String {
